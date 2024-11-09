71f4a0e51fc466527b9398c296198ee5d9061008
FlamingHerb <msbbomisol@gmail.com>
2024-11-10 03:03:36 +0800

    feat: run and reload disallowed Resolves #3

--- scripts/player_body.gd
+++ scripts/player_body.gd
@@ -16,6 +16,7 @@ extends CharacterBody2D
 var meteor_orbit_prefab = preload("res://scenes/player_orbit_follower.tscn")
 var actual_meteor_prefab = preload("res://scenes/meteor.tscn")
 var can_fire = true
+var reloading = false
 
 const SPEED = 200.0
 const ROTATION_SPEED = 2
@@ -28,9 +29,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#region Movement
-	var input_direction = Input.get_vector("player_left", "player_right", "player_up", "player_down")
-	
-	velocity = input_direction.normalized() * SPEED
+	if not reloading:
+		var input_direction = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+		velocity = input_direction.normalized() * SPEED
+	else:
+		velocity = Vector2.ZERO
 	
 	if velocity != Vector2(0, 0):
 		player_sprite.rotation = velocity.angle() - (PI/2)
@@ -46,11 +49,13 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("player_reload"):
 		hold_down_timer.paused = false
 		reload_animation.visible = true
+		reloading = true
 		print("Reloading")
 		
 	if Input.is_action_just_released("player_reload"):
 		hold_down_timer.paused = true
 		reload_animation.visible = false
+		reloading = false
 		print("Not anymore")
 
 func _orbit_spawn_after_timeout() -> void:
